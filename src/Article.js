b38ab597de8ebe99615caa8fe757e40cc19f3ca9
King-OJ <clementojigs@hotmail.com>
2022-01-25 02:15:38 -0800

    first commit

---
+++ src/Article.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import moment from 'moment'
+const Article = ({ title, date, length, snippet}) => {
+  console.log(date)
+  return (
+    <article className="post">
+      <h2>{title}</h2>
+      
+      <div className="post-info">
+        <span>{moment(date).format('dddd Do, MMMM YYYY')}</span>
+        <span>{length} min read</span>
+      </div>
+
+      <p>{snippet}</p>
+
+    </article>
+  )
+}
+
+export default Article
